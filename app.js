655bc623460d248bf7ec8f2e9dcc595a324b7a8f
i8abyte <imransalim08@gmail.com>
2025-09-04 20:21:53 +1200

    Use the rate-limiter to only allow three requests per fifteen minutes to the OpenAI API

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var rateLimiter = require('express-rate-limit');
+
+const limiter = rateLimiter.rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 3,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+  ipv6Subnet: 48,
+});
 
 var indexRouter = require('./routes/index');
 
@@ -18,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(limiter);
 
 app.use('/', indexRouter);
 
